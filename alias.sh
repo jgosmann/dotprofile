f380599c14988865fd776b98f0e3b58d2cda9882
Jan Gosmann <jan@hyper-world.de>
2011-11-08 14:38:31 +0100

    Fix if statement

--- alias.sh
+++ alias.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 alias mysqlstart="sudo /opt/local/bin/mysqld_safe5"
 alias mysqlstop="/opt/local/bin/mysqladmin5 -u root -p shutdown"
-if !(type -p netcat > /dev/null); then
+if ! (type -p netcat > /dev/null); then
     alias netcat=gnetcat
 fi

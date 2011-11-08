3d8b7469e2540b1e4985dc1ea5ca95e763b79ac5
Jan Gosmann <jan@hyper-world.de>
2011-11-08 12:18:53 +0100

    Init netcat alias only if needed.

--- alias.sh
+++ alias.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 alias mysqlstart="sudo /opt/local/bin/mysqld_safe5"
 alias mysqlstop="/opt/local/bin/mysqladmin5 -u root -p shutdown"
-alias netcat=gnetcat
+if !(type -p netcat > /dev/null); then
+    alias netcat=gnetcat
+fi

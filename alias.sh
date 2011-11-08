8144587c1ae9a3942f357097024f4dd72d8ea9e3
Jan Gosmann <jan@hyper-world.de>
2011-11-08 16:20:21 +0100

    Postgres restart alias.

--- alias.sh
+++ alias.sh
@@ -2,4 +2,5 @@
 alias ipython=ipython-2.6
 alias mysqlstart="sudo /opt/local/bin/mysqld_safe5"
 alias mysqlstop="/opt/local/bin/mysqladmin5 -u root -p shutdown"
+alias postgres-restart="sudo -u postgres /Library/PostgreSQL/9.0/bin/pg_ctl -D /Library/PostgreSQL/9.0/data restart"
 alias netcat=gnetcat

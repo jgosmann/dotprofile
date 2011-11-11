cea795f2fb40e92d2811478eec8c936de3bd05d5
Jan Gosmann <jan@hyper-world.de>
2011-11-11 19:46:37 +0100

    Use nc instead of gnetcat if there is no netcat.

--- alias.sh
+++ alias.sh
@@ -3,5 +3,5 @@ alias mysqlstart="sudo /opt/local/bin/mysqld_safe5"
 alias mysqlstop="/opt/local/bin/mysqladmin5 -u root -p shutdown"
 alias postgres-restart="sudo -u postgres /Library/PostgreSQL/9.0/bin/pg_ctl -D /Library/PostgreSQL/9.0/data restart"
 if ! (type -p netcat > /dev/null); then
-    alias netcat=gnetcat
+    alias netcat=nc
 fi
